b4ed7ab4ca5d815ee6d0e263713c268611b77dba
Livio Ribeiro <livioribeiro@outlook.com>
2015-12-12 09:23:51 -0300

    add "feature(custom_attribute)"

--- src/lib.rs
+++ src/lib.rs
@@ -124,7 +124,7 @@
 //! # }
 //! ```
 
-#![cfg_attr(feature = "serde_macros", feature(custom_derive, plugin))]
+#![cfg_attr(feature = "serde_macros", feature(custom_attribute, custom_derive, plugin))]
 #![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
 
 extern crate hyper;
