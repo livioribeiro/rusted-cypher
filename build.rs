431d1d0e46ad9ac2c2c335e0073391b59765fa27
Lon Kastenson <lhkastenson@gmail.com>
2016-07-09 14:11:17 -0400

    Fix serde dependency on syntex

--- build.rs
+++ build.rs
@@ -7,15 +7,12 @@ mod inner {
     use std::path::Path;
 
     pub fn main() {
-        let out_dir = env::var_os("OUT_DIR").unwrap();
+       let out_dir = env::var_os("OUT_DIR").unwrap();
 
        let src = Path::new("src/lib.rs.in");
        let dst = Path::new(&out_dir).join("lib.rs");
 
-       let mut registry = syntex::Registry::new();
-
-       serde_codegen::register(&mut registry);
-       registry.expand("", &src, &dst).unwrap();
+       serde_codegen::expand(&src, &dst).unwrap();
     }
 }
 
